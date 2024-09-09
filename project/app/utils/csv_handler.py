313e825332a52731942d796d69d1b0184db39681
💫 Lic. Avila <aferrerj@uninorte.edu.co>
2024-09-08 22:10:47 -0500

    reverted some changes

--- project/app/utils/csv_handler.py
+++ project/app/utils/csv_handler.py
@@ -1,28 +1,43 @@
 from ..models import *
 from csv import reader
-    
+
 csv_path = "project/app/static/resources/dataset_movies.csv"
 
-def search_in_csv (title: str) -> tuple[bool, list[Movie]]:
+def search_in_csv(title: str) -> tuple[bool, list[Movie]]:
     movies = []
-
-    with open(csv_path, "r", encoding='utf-8') as csv_file:        
+    
+    with open(csv_path, "r", encoding='utf-8') as csv_file:
         csv_reader = reader(csv_file)
-        next(csv_reader)
-
+        next(csv_reader)  
+        
         for row in csv_reader:
             if title.strip().lower() in row[0].strip().lower():
-                movies.append(Movie(row[0],row[1],row[2],row[3],row[4],row[5],row[6]))
+                movie = Movie(
+                    title=row[0],
+                    wwe=float(row[1]),
+                    dome=float(row[2]),
+                    dompe=float(row[3]),
+                    fore=float(row[4]),
+                    forpe=float(row[5]),
+                    year=int(row[6])
+                )
+                movies.append(movie)
+    
+    return (False, []) if not movies else (True, movies)
 
-    return (False, []) if len(movies) == 0 else (True, movies)
-        
 
-def csv_head(base: int = 0, path:str=csv_path) -> list:
-    with open(path, "r") as csv_file:
-        read = reader(csv_file)
-        next(read) 
+def csv_head(base: int = 0, path: str = csv_path) -> list:
+    with open(path, 'r', encoding='utf-8') as csv_file:
+        csv_reader = list(reader(csv_file))[2:] 
         
-        if base == 0:
-            return [next(read)[0]] 
+        movies = [Movie(
+            title=row[0],
+            wwe=float(row[1]),
+            dome=float(row[2]),
+            dompe=float(row[3]),
+            fore=float(row[4]),
+            forpe=float(row[5]),
+            year=int(row[6])
+        ) for row in csv_reader]
         
-        return [next(read)[0] for _ in range(base)]
+        return movies[:base] if base > 0 else movies
